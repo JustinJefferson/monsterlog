ceeac4bd91d7426f7cc05a9623b2c01c84e2e7ef
Justin Jefferson <justleland@gmail.com>
2022-03-12 23:14:57 -0500

    Removing SecretKey

--- monsterlog/settings.py
+++ monsterlog/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@aux(y(zg9nb@fim0+o6vkx6n8i$@s%i)xr(4dl%&+9&!f#ol!'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
