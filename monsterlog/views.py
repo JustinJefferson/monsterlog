85a395a274e02aa43f0e36fc3c4b526d7497a7e8
Justin Jefferson <justleland@gmail.com>
2022-03-30 15:24:29 -0400

    Setup base template, Added bootstrap CDN

--- monsterlog/views.py
+++ monsterlog/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    return render(request, 'monsterlog/base.html')
+    return render(request, 'base.html')
--- monsters/views.py
+++ monsters/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse, Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_list_or_404
 
-from .models import Monster
+from .models import Monster, Evolution
 
 
 def index(request):
-    return HttpResponse("Hello, These are monsters")
+    try:
+        monsters = Monster.objects.all()
+    except Monster.DoesNotExist:
+        raise Http404
+    return render(request, 'monsters/index.html', {'monsters': monsters})
 
 
 def detail(request, monster_id):
@@ -13,4 +17,14 @@ def detail(request, monster_id):
         monster = Monster.objects.get(pk=monster_id)
     except Monster.DoesNotExist:
         raise Http404
-    return render(request, 'monsters/detail.html', {'monster': monster})
+
+    pre_evolution = None
+    pre_evolutions = Evolution.objects.filter(next_evolution=monster)
+    if pre_evolutions:
+        pre_evolution = pre_evolutions[0]
+    evolutions = Evolution.objects.filter(pre_evolution=monster)
+    return render(request, 'monsters/detail.html', {
+        'monster': monster,
+        'pre_evolution': pre_evolution,
+        'evolutions': evolutions
+    })
